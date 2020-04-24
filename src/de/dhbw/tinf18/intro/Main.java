c038834229642672c6972d066acabeb79e2fe399
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2020-04-24 07:59:29 +0200

    Vorbereiten des Java-Projekts

---
+++ src/de/dhbw/tinf18/intro/Main.java
@@ -0,0 +1,8 @@
+package de.dhbw.tinf18.intro;
+
+public class Main {
+
+	public static void main(String[] arguments) {
+		System.out.println("Hello, TINF18B1, TINF18B2, TINF18B4 and TINF18B5");
+	}
+}
